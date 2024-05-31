4284fb280d53bdb511b47a18cd88ad0acc0fb84c
Fekloduv <64400957+Fekloduv@users.noreply.github.com>
2024-05-31 11:54:01 +0300

    Update my_config_reader.py

--- my_config_reader.py
+++ my_config_reader.py
@@ -3,16 +3,20 @@ from config_exception import ConfigException
 
 class MyConfigReader(ConfigReaderInterface):
     def read_config(self, filepath):
+        config_path = Path(filepath)
+        if not config_path.is_file():
+            raise ConfigException(f"Файл конфигурации {config_path} не существует.")
         config = {}
         try:
-            with open(filepath, 'r') as file:
+            with config_path.open('r') as file:
                 for line in file:
                     name, value = line.strip().split('=')
                     config[name.strip()] = value.strip()
+            buffer_size = config.get('buffer_size')
+            if not buffer_size.isdigit() or int(buffer_size) <= 0:
+                raise ConfigException("Некорректный размер буфера в конфигурационном файле.")
             return config
         except ValueError as e:
             raise ConfigException(f"Ошибка форматирования в конфигурационном файле: {str(e)}")
-        except FileNotFoundError as e:
-            raise ConfigException(f"Файл конфигурации не найден: {str(e)}")
         except Exception as e:
             raise ConfigException(f"Неизвестная ошибка при чтении конфигурационного файла: {str(e)}")
