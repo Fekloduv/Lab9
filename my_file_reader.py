507bd592b68eed55c4c9c63e23d5208ab4cf86c1
Fekloduv <64400957+Fekloduv@users.noreply.github.com>
2024-05-31 11:53:43 +0300

    Update my_file_reader.py

--- my_file_reader.py
+++ my_file_reader.py
@@ -8,8 +8,10 @@ class MyFileReader(FileReaderInterface):
     def read_file(self, buffer_size):
         if buffer_size <= 0:
             raise ConfigException("Размер буфера должен быть положительным числом.")
+        if not self.file_path.is_file():
+            raise ConfigException(f"Файл {self.file_path} не существует.")
         try:
-            with open(self.file_path, 'r') as file:
+            with self.file_path.open('r') as file:
                 while True:
                     chunk = file.read(buffer_size)
                     if not chunk:
